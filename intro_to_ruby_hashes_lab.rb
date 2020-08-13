a0b0409c53130537e52517a29bb1a7fdc2d99ab0
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-13 23:17:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,8 +21,8 @@ def monopoly_with_third_tier
   # complexity.
   {
     railroads: {
-      pieces: "4",
-      rent_in_dollars: {}
+      pieces: 4,
+      rent_in_dollars: {},
       name: {}
     
     }
