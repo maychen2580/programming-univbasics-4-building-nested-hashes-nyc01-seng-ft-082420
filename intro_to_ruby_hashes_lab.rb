0c6e691e11a74890054c227b697b5fd092fe557e
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-13 23:51:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -64,9 +64,9 @@ def monopoly_with_fourth_tier
         four_pieces_owned: 200,
       },
       names: {
-        reading_railroad: {},
+        reading_railroad: {mortgage_value: 100},
         pennsylvania_railroad: {mortagae_value: 200},
-        b_and_o_railroad:{},
+        b_and_o_railroad:{mortgage_value: 400},
         shortline_railroad:{},
         
       }
