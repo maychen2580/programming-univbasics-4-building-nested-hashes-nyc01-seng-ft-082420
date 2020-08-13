8f1f6eb9582f4e70e009bdce938ae74f24bcc649
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-13 23:25:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,9 @@ def monopoly_with_third_tier
       pieces: 4,
       rent_in_dollars: {
         one_piece_owned: 25,
-        two_picec_owned: 50,
+        two_piece_owned: 50,
+        three_piece_owned: 100,
+        fout_piece_owned: 200
       },
       name: {}
     
